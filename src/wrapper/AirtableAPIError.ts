d8d541f5a131d7c12373a9e77d1c1ce938821d3b
Charalampos Fanoulis <yo@fanoulis.dev>
2022-03-09 23:30:59 +0200

    fix: use proper returned JSON property

--- src/wrapper/AirtableAPIError.ts
+++ src/wrapper/AirtableAPIError.ts
@@ -1,10 +1,10 @@
 export class AirtableAPIError extends Error {
 	public constructor(public errorCode: number, public errorResponse: AirtableErrorResponse) {
-		super(`Airtable returned error code ${errorCode}: [${errorResponse.code}] ${errorResponse.message}`);
+		super(`Airtable returned error code ${errorCode}: [${errorResponse.type}] ${errorResponse.message}`);
 	}
 }
 
 export interface AirtableErrorResponse {
 	message: string;
-	code: number;
+	type: number;
 }
